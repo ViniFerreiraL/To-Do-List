2ecf0a66290e199ec90e03915e465237fbafb256
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-07-15 17:04:15 -0300

    creat new home

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const TaskController = require("../Controller/TaskController");
-router.get("/home", TaskController.getAllTask);
+router.get("/", TaskController.getAllTask);
 router.post("/create", TaskController.createTask);
 router.get("/getById/:id/:method", TaskController.GetById);
 router.post("/updateOne/:id", TaskController.updateOneTask);
